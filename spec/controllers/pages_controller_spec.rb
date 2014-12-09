05dd523c2b8286e4cf39a56f29d0476420b3e7fd
EnferTat <thibautbqn@gmail.com>
2014-12-09 15:10:55 +0100

    Fini avec les pages statiques

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, :type => :controller do
+  render_views
 
   describe "GET home" do
     it "returns http success" do
       get :home
       expect(response).to have_http_status(:success)
     end
+
+    it "doit avoir le bon titre" do
+      get :home
+      expect(response).to have_selector("h1", text: "Accueil")
+    end
+
   end
 
   describe "GET contact" do
@@ -14,6 +21,25 @@ RSpec.describe PagesController, :type => :controller do
       get :contact
       expect(response).to have_http_status(:success)
     end
+
+    it "doit avoir le bon titre" do
+      get :contact
+      expect(response).to have_selector("h1", text: "Contact")
+    end
+
+  end
+
+  describe "GET 'about'" do
+    it "devrait réussir" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+
+    it "doit avoir le bon titre" do
+      get :about
+      expect(response).to have_selector("h1", text: "À propos")
+    end
+
   end
 
 end
